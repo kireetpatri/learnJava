4e75feeedbb87cb7abb6eb4dd5b61c9723b3c549
kireetpatri <41479751+kireetpatri@users.noreply.github.com>
2018-07-20 16:32:04 +0200

    for loop reqd

--- learnJava/src/main/java/projectOne/jsonParser.java
+++ learnJava/src/main/java/projectOne/jsonParser.java
@@ -1,6 +1,7 @@
 package projectOne;
 import java.io.File;
 
+import java.util.Iterator;
 import java.util.Scanner;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -8,6 +9,7 @@ import java.io.FileReader;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+
 
 
 
@@ -28,10 +30,19 @@ public class jsonParser {
             JSONObject testJsonObj = (JSONObject) fileObj;
             //Read the root and print it
             String grandfather = (String) testJsonObj.get("name");
+            //Loop the child of the root node
             JSONArray fathers = (JSONArray) testJsonObj.get("children");
-            for(Object father:fathers){
-                JSONArray children = (JSONArray) father.get
+            Iterator<String> iterator = fathers.iterator();
+            //put a for loop for length of array
+            while (iterator.hasNext()){
+
+                JSONArray children = (JSONArray) fathers.get(0);
+
+
+
             }
+
+
             //System.out.println("The path is "+jsonFilePath);
         }
         catch(FileNotFoundException fe) {
